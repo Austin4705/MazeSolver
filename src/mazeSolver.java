236856e97a86d67817a44130cf9386bdc5025bb6
Austin <austin4705@gmail.com>
2022-05-23 18:02:58 -0700

    dijkstra revert

--- src/mazeSolver.java
+++ src/mazeSolver.java
@@ -9,6 +9,7 @@ public class mazeSolver{
         //while the algorithm is programmed to try and connect all start and end nodes together,
         // it doesn't necessary do multi starts in the most efficient way, just guarantees it will find a solution if
         //one is out there. Tries and guarentees most effective single case solution still
+        //in the future i would like to implement A* with a geometric distance heuristic. currently i think dijkstras will suffice for the project
         for(node start : mazeData.getInstance().adjListS.values()){
             for(node end : mazeData.getInstance().adjListE.values()){
                 //the actual dijkstras implementation is based off of Baeldung article on it
@@ -24,26 +25,5 @@ public class mazeSolver{
         }
     }
 
-    //dijkstras helper function
-    private static node getLowDist(Set<node> unsettledNodes){
-        node lowDistNode = null;
-        int lowDist = Integer.MAX_VALUE;
-        for(node n : unsettledNodes){
-            int nodeDist = n.dist;
-            if(nodeDist < lowDist){
-                lowDist = nodeDist;
-                lowDistNode = n;
-            }
-        }
-        return lowDistNode;
-    }
 
-    private static void calcMinDist(node evalN, node source, int edgeWeight){
-        //weight is 1, remember I went with a voxel based system. In a 2d world i can do weight and intersection system but with n dimensions
-        //it gets trickier to do so
-        int sourceDist = source.dist;
-        if(sourceDist + edgeWeight < evalN.dist){
-            evalN.set
-        }
-    }
 }
